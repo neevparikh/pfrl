f8bcd90cb3801faf15b4f8e4d6e3cd87bcd11eb0
Neev Parikh <neev_parikh@brown.edu>
2021-09-24 22:36:05 -0400

    Updating RND

--- pfrl/nn/rnd.py
+++ pfrl/nn/rnd.py
@@ -2,12 +2,64 @@ from copy import deepcopy
 from logging import getLogger
 
 import torch
+from torch import nn
+from torch.nn import init
 import numpy as np
 import gym
 
 from pfrl.initializers import init_xavier_uniform
-from pfrl.nn import EmpiricalNormalization
+from pfrl.nn import EmpiricalNormalization, Lambda
 from pfrl.env import VectorEnv, Env
+
+
+class RNDModel(torch.nn.Module):
+    """
+    Torch RND architecture taken from https://github.com/jcwleo/random-network-distillation-pytorch/blob/master/model.py
+    """
+    def __init__(self, input_size, frame_stack=4, output_size=512):
+        super(RNDModel, self).__init__()
+
+        self.input_size = input_size
+        self.output_size = output_size
+
+        feature_output = 7 * 7 * 64
+        self.predictor = nn.Sequential(
+            nn.Conv2d(in_channels=frame_stack, out_channels=32, kernel_size=8, stride=4),
+            nn.LeakyReLU(),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2),
+            nn.LeakyReLU(),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1),
+            nn.LeakyReLU(),
+            Lambda(lambda x: x.view(x.size(0), -1)),
+            nn.Linear(feature_output, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512))
+
+        self.target = nn.Sequential(
+            nn.Conv2d(in_channels=frame_stack, out_channels=32, kernel_size=8, stride=4),
+            nn.LeakyReLU(),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2),
+            nn.LeakyReLU(),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1),
+            nn.LeakyReLU(),
+            Lambda(lambda x: x.view(x.size(0), -1)),
+            nn.Linear(feature_output, 512))
+
+        for p in self.modules():
+            if isinstance(p, nn.Conv2d):
+                init.orthogonal_(p.weight, np.sqrt(2))
+                p.bias.data.zero_()
+
+            if isinstance(p, nn.Linear):
+                init.orthogonal_(p.weight, np.sqrt(2))
+                p.bias.data.zero_()
+
+    def forward(self):
+        raise NotImplementedError(
+            "Should not call forward on this module, see module.predictor, module.target")
+
 
 class RND(torch.nn.Module):
     """Implements Random Network Distillation.
@@ -20,7 +72,8 @@ class RND(torch.nn.Module):
         gpu (int): GPU device id if not None nor negative
         optimizer (torch.optim.Optimizer): Optimizer used to train the model, default is Adam
     """
-    def __init__(self,
+    def __init__(
+            self,
             model,
             obs_shape,
             init_steps,
@@ -32,8 +85,8 @@ class RND(torch.nn.Module):
         self.init_steps = init_steps
         self.obs_shape = obs_shape
 
-        self.target = model
-        self.predictor = deepcopy(self.target)
+        self.target = model.target
+        self.predictor = model.predictor  # deepcopy(self.target)
 
         self.target.apply(init_xavier_uniform)
         self.predictor.apply(init_xavier_uniform)
@@ -82,14 +135,17 @@ class RND(torch.nn.Module):
         predicted_vector = self.predictor(states)
         target_vector = self.target(states)
 
-        intrinsic_reward = torch.nn.functional.mse_loss(predicted_vector, target_vector, reduction='mean')
-        intrinsic_reward = intrinsic_reward.unsqueeze(0) # dim: 1,
+        intrinsic_reward = torch.nn.functional.mse_loss(predicted_vector,
+                                                        target_vector,
+                                                        reduction='mean')
+        intrinsic_reward = intrinsic_reward.unsqueeze(0)  # dim: 1,
         intrinsic_reward = self.reward_normalizer(intrinsic_reward)
 
         loss = intrinsic_reward.mean(dim=0)
 
         if log:
-            self.logger.debug('int_rew: %f, rnd_loss: %f, mean: %f, std: %f',
+            self.logger.debug(
+                'int_rew: %f, rnd_loss: %f, mean: %f, std: %f',
                 loss.item(),
                 intrinsic_reward.mean().item(),
                 self.reward_normalizer.mean.item(),
